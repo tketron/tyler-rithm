af23a70385dea38c8f69ced9d334c59cb57c1a41
Tyler Ketron <tketron@gmail.com>
2018-06-05 12:03:24 -0700

    basic html and javascript functionality

---
+++ week-2/giphy-party/javascript.js
@@ -0,0 +1,30 @@
+const KEY = 'HEg4D4yn5yL1hDohN0wlZ9FnVpwrWnm5';
+const GIPHY_SEARCH_URL = 'http://api.giphy.com/v1/gifs/random';
+
+$(function() {
+  $('#submitForm').on('submit', function(event) {
+    event.preventDefault();
+    addNewGif();
+  });
+  $('#clearBtn').on('click', clearGIFs);
+});
+
+function addNewGif() {
+  const searchString = $('#searchText').val();
+  getGIF(searchString);
+}
+
+function clearGIFs() {
+  $('#gif-container').empty();
+}
+
+function getGIF(string) {
+  let address = `${GIPHY_SEARCH_URL}?tag=${string}&api_key=${KEY}`;
+  $.get(address, function(data) {
+    console.log(data.data);
+    // console.log(data.data.url);
+    // return data.data.embed_url;
+    let gifImage = data.data.image_url;
+    $('#gif-container').prepend(`<div><img src='${gifImage}'></div>`);
+  });
+}
