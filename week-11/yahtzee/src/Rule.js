090869f66bf9b9a12cf85dd31ce6484f5c129aa1
Tyler Ketron <tketron@gmail.com>
2018-08-10 09:49:05 -0700

    added yahtzee

---
+++ week-11/yahtzee/src/Rule.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Rule.css'
+
+class Rule extends Component {
+  render() {
+      return (
+        <tr className="Rule Rule-active" onClick={this.props.doScore}>
+          <td className="Rule-name">{this.props.name}</td>
+          <td className="Rule-score">{this.props.score}</td>
+        </tr>
+      )
+  }
+}
+
+export default Rule;
